67f5707427eda9ec4e7ffbf9c81600bbacc9e0ed
Blake Gotting and Ngan Nguyen <ngan@>
2018-11-01 16:06:22 -0700

    add alert for winner

--- js/scripts.js
+++ js/scripts.js
@@ -4,30 +4,32 @@ function Game(){
   this.players = [];
 }
 
- Game.prototype.addPlayer = function(person) {
-   this.players.push(person)
- }
+Game.prototype.addPlayer = function(person) {
+  this.players.push(person)
+}
 
 // Business Logic for Player
 
-function Person (name, score, total, final){
+function Person (name, score, total, final, sum){
   this.name = name;
   this.score = [];
   this.total= total;
   this.finalTotal = [];
+  this.sum = sum
 }
 
 Person.prototype.onRoll = function () {
   var randomNumber = Math.floor((Math.random() * 6) + 1);
-    if (randomNumber === 1 ){
-      alert("Next Player");
-      this.score.push(1);
-    }
-    else if (( randomNumber > 1 ) && ( randomNumber <= 6)){
-     this.score.push(randomNumber);
-    }
-    console.log(this.score);
+  if (randomNumber === 1 ){
+    alert("Next Player");
+    this.score.push(1);
   }
+  else if (( randomNumber > 1 ) && ( randomNumber <= 6)){
+    this.score.push(randomNumber);
+  }
+  console.log(this.score);
+  return randomNumber;
+}
 
 Person.prototype.turnScore = function() {
   var total = 0;
@@ -45,55 +47,65 @@ Person.prototype.turnScore = function() {
 }
 
 Person.prototype.totalScore = function() {
- this.finalTotal.push(this.total);
- console.log(this.total)
- this.total = 0;
+  this.finalTotal.push(this.total);
+  console.log(this.total)
+  this.total = 0;
+  return this.finalTotal;
 }
 
-Person.prototype.sumScore = function () {
+Person.prototype.finalScore = function () {
+  var total = 0
   for( var i = 0; i <this.finalTotal.length; i+= 1) {
-    if (finalTotal >= 100) {
-      return alert("You win! Congratulations.");
-    }
-    console.log(this.total);
-    console.log(this.finalTotal);
+    this.sum = total += this.finalTotal[i]
+    // return alert("You win! Congratulations.");
   }
+  return this.sum;
 }
 
 var pigDice = new Game();
-var player1 = new Person("Blake")
-var player2 = new Person("Ngan")
+var player1 = new Person();
+var player2 = new Person();
+
 pigDice.addPlayer(player1)
 pigDice.addPlayer(player2)
-// player1
-// player1.addScore()
-// player2.addScore()
 
 // User logic for Game
 function rollingDice() {
-  var display = [];
   $("#rollPlayerOne").click(function(){
-    player1.onRoll();
+    var seeScore = player1.onRoll();
+    $("#playerOneScore").text(seeScore);
+    // console.log("look", seeScore)
   });
+
   $("#holdPlayerOne").click(function(){
     var test = player1.turnScore();
-    var string = test.toString()
-    // console.log("look",string)
+    var string = test.toString();
+    var count = player1.totalScore();
+    var allOne = player1.finalScore();
     $("#playerOneTurnTotal").text(string);
+    $("#playerOneTotalScore").text(allOne);
+    if (allOne >= 100) {
+      alert("You win! Congratulations.");
+    }
+  });
 
-  });
   $("#rollPlayerTwo").click(function(){
-    player2.onRoll();
+    var seeScore = player2.onRoll();
+    $("#playerTwoScore").text(seeScore);
   });
+
   $("#holdPlayerTwo").click(function(){
     var test = player2.turnScore();
     var string = test.toString()
-    // console.log("look",string)
+    var count = player2.totalScore();
+    var allTwo = player2.finalScore();
     $("#playerTwoTurnTotal").text(string);
+    $("#playerTwoTotalScore").text(allTwo);
+    if (allTwo >= 100) {
+      alert("You win! Congratulations.");
+    }
   });
-  // console.log(this.score);
 };
-
 
 
 

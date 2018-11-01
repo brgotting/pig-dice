47abac8273e123eb2fe5873a211a98917d59f9ef
Blake Gotting and Ngan Nguyen <blake+ngan@>
2018-11-01 09:34:53 -0700

    Make win function.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ##Specifications
-- Random number generator from 1 to 6 base on die
+- Random number generator from 1 to 6 based on di
 - If player scores a 1, they score nothing and this end their turn
   - number === 1 , lose turn
 - If the player rolls any other number > 1
@@ -7,7 +7,7 @@
   - turn continues
 - Two players
   - create as object and nest score in the object
-- If a player chooses to "hold", their turn total is added to their score, and it becomes the next player's turn.
+- If a player chooses to "hold", their total is added to the score, and it becomes the next player's turn.
   - Add roll button
   - Add stop button (count the total and store the information)
 - The first player to score 100 or more points wins.
--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,11 @@
 //
 // function randomNumber(number){
 //   var number = Math.floor((Math.random() * 6) + 1);
-//     score.push(RollingTurn(number));
+//     score.push(rollingTurn(number));
 //     console.log("total", score);
 // }
 
-// function RollingTurn(number) {
+// function rollingTurn(number) {
 //   if (number === 1 ){
 //     return score = 0
 //   }
@@ -39,28 +39,33 @@ function Game(){
 function Person (name, score, total){
   this.name = name;
   this.score = [];
-  this.totalScore= total;
+  this.totalScore= [total];
 }
 
 Person.prototype.onRoll = function randomNumber(number){
   var number = Math.floor((Math.random() * 6) + 1);
-    this.score.push(RollingTurn(number));
+    this.score.push(rollingTurn(number));
 }
 
-function RollingTurn(number) {
+function rollingTurn(number) {
   if (number === 1 ){
-    return number = 0 ; alert("Next PLayer");
+    return alert("Next Player");
   }
   else if (( number > 1 ) && ( number <=6)){
    return number;
   }
 }
 
+function win(totalScore) {
+  if (totalScore >= 100) {
+    return alert("You win! Congratulations.");
+  }
+}
+
 Person.prototype.addScore = function() {
   var total = 0;
   for( var i = 0; i <this.score.length; i+= 1) {
-    //if array has 0, score = 0
-    if(this.score.includes(0)) {
+    if(this.score.includes(undefined)) {
       this.totalScore = 0
     }
     else  {
@@ -72,14 +77,13 @@ Person.prototype.addScore = function() {
 
 
 var pigDice = new Game();
-var player1 = new Person("Blake", 3)
-var player2 = new Person("Ngan", 100)
+var player1 = new Person("Blake")
+var player2 = new Person("Ngan")
 pigDice.addPlayer(player1)
 pigDice.addPlayer(player2)
-player1.onRoll()
-player1.onRoll()
-player1.onRoll()
 player1
+// player1.addScore()
+// player2.addScore()
 
 
 
@@ -91,5 +95,6 @@ player1
 
 // User logic for Game
 $(document).ready(function() {
+
 
 });
